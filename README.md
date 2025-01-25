94cd33f173a8fe8e0bb34696dc74579471a5f2ee
Rex Wang <cping.wang@gmail.com>
2025-01-26 01:09:06 +0800

    Added calculator.feature and modified StepDefinitions.class

--- README.md
+++ README.md
@@ -18,3 +18,22 @@ mvn archetype:generate                     \
 
 5. 查看 StepDefinitions.class 內容，此為 cucumber 根據 example.feature 建立
 ![截圖 2025-01-26 凌晨12 46 00](https://github.com/user-attachments/assets/85ab0ff0-1ee2-4dd1-8175-16f9c0a65685)
+
+6. 刪除 example.feature ，新增 calculator.feature，內容如下：
+```gherkin
+Feature: Calculator
+
+  Scenario: Add two numbers
+    Given the first number is 50
+    And the second number is 70
+    When the two numbers are added
+    Then the result should be 120
+```
+7. 執行 maven 測試指令如下，並查看結果，取得 cucumber 提示，，表示  calculator.feature 已被讀取
+```gherkin
+mvn test
+```
+![截圖 2025-01-26 凌晨1.02.12.png](..%2F..%2F..%2F..%2FDesktop%2F%E6%88%AA%E5%9C%96%202025-01-26%20%E5%87%8C%E6%99%A81.02.12.png)
+
+8. 將提示內容覆蓋於 StepDefinitions.class，再執行一次 Maven 測試指令會看到不ㄧ樣的提示內容，表示  StepDefinitions.class 已被讀取
+![截圖 2025-01-26 凌晨1.05.41.png](..%2F..%2F..%2F..%2FDesktop%2F%E6%88%AA%E5%9C%96%202025-01-26%20%E5%87%8C%E6%99%A81.05.41.png)
--- src/test/java/calculator/StepDefinitions.java
+++ src/test/java/calculator/StepDefinitions.java
@@ -6,16 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("the first number is {int}")
+    public void the_first_number_is(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("the second number is {int}")
+    public void the_second_number_is(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the two numbers are added")
+    public void the_two_numbers_are_added() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the result should be {int}")
+    public void the_result_should_be(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
-    }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-    }
 
 }
